75929f16d07a59cf1a243d066c2b315f80114bf1
zacharywatson452 <Zacharywatson452@gmail.com>
2017-07-26 10:41:48 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,136 @@
+function textRed(i){
+if(i=="0"){ 
+	document.getElementById("link").style.color = "red";
+} 
+else if(i=="1"){
+	document.getElementById("link1").style.color = "red";
+}
+else if(i=="2"){
+	document.getElementById("link2").style.color = "red";
+}
+else if(i=="3"){
+	document.getElementById("link3").style.color = "red";
+}
+else if(i=="4"){
+	document.getElementById("link4").style.color = "red";
+}
+else if(i=="5"){
+	document.getElementById("link5").style.color = "red";
+}
+else if(i=="6"){
+	document.getElementById("link6").style.color = "red";
+}
+else if(i=="7"){
+	document.getElementById("link7").style.color = "red";
+}
+else if(i=="8"){
+	document.getElementById("link8").style.color = "red";
+}
+else if(i=="9"){
+	document.getElementById("link9").style.color = "red";
+}
+else if(i=="10"){
+	document.getElementById("link10").style.color = "red";
+}
+else if(i=="11"){
+	document.getElementById("link11").style.color = "red";
+}
+else if(i=="12"){
+	document.getElementById("link12").style.color = "red";
+}
+else if(i=="13"){
+	document.getElementById("link13").style.color = "red";
+}
+else if(i=="14"){
+	document.getElementById("link14").style.color = "red";
+}
+else if(i=="15"){
+	document.getElementById("link15").style.color = "red";
+}
+else if(i=="16"){
+	document.getElementById("link16").style.color = "red";
+}
+else if(i=="17"){
+	document.getElementById("link17").style.color = "red";
+}
+else if(i=="18"){
+	document.getElementById("link18").style.color = "red";
+}
+else if(i=="19"){
+	document.getElementById("link19").style.color = "red";
+}
+else if(i=="20"){
+	document.getElementById("link20").style.color = "red";
+}
+}
+
+function textBlack(i){
+
+if(i=="0"){
+	document.getElementById("link").style.color = "black";
+}
+else if(i=="1"){
+	document.getElementById("link1").style.color = "black";
+}
+else if(i=="2"){
+	document.getElementById("link2").style.color = "black";
+}
+else if(i=="3"){
+	document.getElementById("link3").style.color = "black";
+}
+else if(i=="4"){
+	document.getElementById("link4").style.color = "black";
+}
+else if(i=="5"){
+	document.getElementById("link5").style.color = "black";
+}
+else if(i=="6"){
+	document.getElementById("link6").style.color = "black";
+}
+else if(i=="7"){
+	document.getElementById("link7").style.color = "black";
+}
+else if(i=="8"){
+	document.getElementById("link8").style.color = "black";
+}
+else if(i=="9"){
+	document.getElementById("link9").style.color = "black";
+}
+else if(i=="10"){
+	document.getElementById("link10").style.color = "black";
+}
+else if(i=="11"){
+	document.getElementById("link11").style.color = "black";
+}
+else if(i=="12"){
+	document.getElementById("link12").style.color = "black";
+}
+else if(i=="13"){
+	document.getElementById("link13").style.color = "black";
+}
+else if(i=="14"){
+	document.getElementById("link14").style.color = "black";
+}
+else if(i=="15"){
+	document.getElementById("link15").style.color = "black";
+}
+else if(i=="16"){
+	document.getElementById("link16").style.color = "black";
+}
+else if(i=="17"){
+	document.getElementById("link17").style.color = "black";
+}
+else if(i=="18"){
+	document.getElementById("link18").style.color = "black";
+}
+else if(i=="19"){
+	document.getElementById("link19").style.color = "black";
+}
+else if(i=="20"){
+	document.getElementById("link20").style.color = "black";
+}
+}
+
+function validations(){
+	
+}
